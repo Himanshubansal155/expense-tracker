8b0cbd61cc17a107203baf689642ef9cf1168aea
Himanshu Bansal <himanshubansal155@gmail.com>
2022-03-13 16:32:41 +0530

    login api changes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { ME_LOGIN } from "../../constants/action.constants";
 import Input from "../shared components/Input/Input";
 
 const Login = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch({
       type: ME_LOGIN,
@@ -13,6 +15,7 @@ const Login = () => {
         password: "123456",
       },
     });
+    navigate("/");
   }, []);
   return (
     <div>
