014f73cee7193f22206484e63484ecd44c688b86
Lakshya Yadav <lakshya.yadav_cs19@gla.ac.in>
2022-03-17 20:43:40 +0530

    formik validation added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,10 +4,24 @@ import { useNavigate } from "react-router-dom";
 import { ME_LOGIN } from "../../constants/action.constants";
 import ButtonField from "../shared components/Button/Button";
 import Input from "../shared components/Input/Input";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .max(30, "Must be 30 characters or less")
+        .required("Required"),
+    }),
+    onSubmit: (values) => {},
+  });
   // useEffect(() => {
   //   dispatch({
   //     type: ME_LOGIN,
@@ -28,16 +42,36 @@ const Login = () => {
       <div className=" h-screen bg-white w-3/5 ">
         <div className=" h-3/4 w-3/4 float-left bg-white mt-20 shadow-2xl">
           <div className="">
-            <form action="#" className="flex flex-col w-full items-center">
+            <form
+              action="#"
+              className="flex flex-col w-full items-center"
+              onSubmit={formik.handleSubmit}
+            >
               <div className=" mt-24">
                 <h1 className="text-black text-2xl ">Log In</h1>
               </div>
 
               <div className="form-group w-1/2  mt-10">
-                <Input type="text" label="Username" required />
+                <Input
+                  type="text"
+                  label="Username"
+                  required
+                  onchange={formik.handleChange}
+                  value={formik.values.username}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.touched.username && formik.errors.username ? (
+                  <p>{formik.errors.username}</p>
+                ) : null}
               </div>
               <div className="form-group w-1/2  mt-10">
-                <Input type="password" label="Password" required />
+                <Input
+                  type="password"
+                  label="Password"
+                  required
+                  onchange={formik.handleChange}
+                  value={formik.values.password}
+                />
               </div>
               <div>
                 <a className="no-underline float-right" href="/">
