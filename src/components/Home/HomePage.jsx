ec40cf8e6f841e74947385a0a6620e936553e698
Himanshu Bansal <himanshubansal155@gmail.com>
2022-04-09 20:51:17 +0530

    pdf creator added with with viewer style

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import PDFDocument from "../shared components/PDFDocument/PDFDocument";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <PDFDocument />
+    </div>
+  );
 };
 
 export default HomePage;
